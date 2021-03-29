b9dd5d209bbeed1d97d9cdd25d1d4a71ebd19d08
Martin Stone <martindstone@me.com>
2021-03-29 12:45:18 -0400

    user:incidents relation

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,19 +1,25 @@
 class IncidentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_current_user
 
   def index
+    @incidents = current_user.incidents
   end
 
   def new
   end
 
   def create
-    i = Incident.new(params.permit(:omg_id, :omg_title, :drill_tester_email, :workspace_bugs, :gcp_bugs))
+    i = current_user.incidents.new(params.permit(:omg_id, :omg_title, :drill_tester_email, :workspace_bugs, :gcp_bugs))
     i.save
     redirect_to incident_url(i)
   end
 
   def show
     @incident = Incident.find(params[:id])
+  end
+
+  def get_current_user
+    @user = current_user
   end
 end
